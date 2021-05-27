64ed1c2697f9ca80b829bbd5e95cad8ab4a6f056
Andrey_s <krd1@list.ru>
2021-05-27 21:10:26 +0300

    Домашнее задание(подсказки)

---
+++ main.py
@@ -0,0 +1,115 @@
+import turtle
+
+
+
+
+player = turtle.Turtle()  # Создать черепаху
+# player.forward(100)  # 100 шагов вперед
+player.shape("turtle")
+player.lt(180)
+player.down()
+player.fd(20)
+player.rt(90)
+player.fd(10)
+player.rt(90)
+player.fd(10)
+player.lt(90)
+player.fd(10)
+player.rt(90)
+player.fd(10)
+player.lt(90)
+player.fd(10)
+player.rt(90)
+player.fd(10)
+player.lt(90)
+player.fd(40)
+player.rt(90)
+player.fd(10)
+player.rt(90)
+player.fd(30)
+player.lt(90)
+player.fd(10)
+player.lt(90)
+player.fd(10)
+player.rt(90)
+player.fd(10)
+player.lt(90)
+player.fd(10)
+player.rt(90)
+player.fd(10)
+player.rt(90)
+player.fd(10)
+player.lt(90)
+player.fd(10)
+player.rt(90)
+player.fd(10)
+player.lt(90)
+player.fd(10)
+player.rt(90)
+player.fd(10)
+player.lt(90)
+player.fd(20)
+player.rt(90)
+player.fd(10)
+player.lt(90)
+player.fd(10)
+player.rt(90)
+player.fd(10)
+player.lt(90)
+player.fd(10)
+player.rt(90)
+player.fd(10)
+player.rt(90)
+player.fd(110)
+player.lt(90)
+player.fd(70)
+player.lt(90)
+player.fd(90)
+player.lt(90)
+player.fd(70)
+
+player.rt(180)
+player.up()
+player.fd(20)
+player.rt(90)
+player.fd(60)
+player.down()
+player.fd(20)
+player.lt(90)
+player.fd(20)
+player.lt(90)
+player.fd(20)
+player.lt(90)
+player.fd(20)
+
+player.rt(90)
+player.up()
+player.fd(20)
+player.rt(90)
+player.fd(10)
+player.down()
+player.fd(40)
+player.lt(90)
+player.fd(20)
+player.lt(90)
+player.fd(40)
+player.lt(90)
+player.fd(20)
+
+
+player.hideturtle()  # спрятать черепаху
+# player.left(90)  # Повернуть на лево 90 градусов
+# player.fd(100)  # вперед 100 шагов
+# player.lt(90)  # налево 90 градусов
+# player.up()  # поднять карандаш
+# player.down()  # опустить карандаш
+# player.rt(90)  # направо 90 градусов
+# player.up()
+# player.fd(100)
+
+
+
+
+
+
+turtle.done()  # Задержать результат
---
+++ postcard.py
@@ -0,0 +1,18 @@
+import  turtle
+
+post = turtle.Turtle()
+
+post.up()
+post.fd(100)
+post.down()
+post.lt(90)
+post.fd(100)
+post.lt(90)
+post.fd(100)
+post.lt(90)
+post.fd(100)
+post.lt(90)
+post.fd(100)
+
+
+turtle.done()
