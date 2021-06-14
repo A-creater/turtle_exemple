c12b4709e7000df8192ff78af63a89c8df14bbcc
Andrey_S <krd1@list.ru>
2021-06-14 15:12:23 +0300

    оптимизировал

--- test.py
+++ test.py
@@ -3,21 +3,20 @@ import  math
 
 play = turtle.Turtle()
 
-def test(iter, fd, lt):
+
+def test(iter, iter1, fd, lt):
     for i in range(iter):
         play.fd(fd)
         play.lt(lt)
-
     play.lt(90)
     play.up()
     play.fd(20)
     play.down()
     play.rt(90)
 
-def test1(iter, fd, lt):
-    for j in range(iter):
+    for j in range(iter1):
         play.fd(fd)
-        play.lt(lt)
+        play.lt(lt1)
 
 
 iterations = 40
@@ -26,6 +25,7 @@ fd = 10
 lt = 360/iterations
 lt1 = 360/iterations1
 
-test(iterations, fd, lt)
-test1(iterations1, fd, lt1)
+test(iterations, iterations1, fd, lt)
+
+
 turtle.done()
